d44b5364f6879f78462e8a989bbc3dd6933cf35b
sivajikalluru <79970157+sivajikalluru@users.noreply.github.com>
2021-11-18 10:23:43 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "myvpc"
+  }
+}
